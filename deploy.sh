b65ce575204467b63bec15d1a7d972e375773c78
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-04-10 18:12:05 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git pull
 mvn package -Dquarkus.package.type=uber-jar
 cp ./target/weblog-1.0-SNAPSHOT-runner.jar ~/apps
 kill -15 $(ps aux | grep weblog-1.0-SNAPSHOT-runner.jar | awk '{print $2}')
-java -jar ~/apps/weblog-1.0-SNAPSHOT-runner.jar
+nohup java -jar ~/apps/weblog-1.0-SNAPSHOT-runner.jar > ~/apps/weblog.log 2>&1 &
