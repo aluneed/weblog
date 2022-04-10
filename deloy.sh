5fa12cb480fc0f83a0ff15167e335cbe64535b87
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-04-10 15:35:58 +0800

    update deploy.sh: kill web-app before restart

--- deloy.sh
+++ deloy.sh
@@ -1,4 +1,5 @@
 git pull
 mvn package -Dquarkus.package.type=uber-jar
 cp ./target/weblog-1.0-SNAPSHOT-runner.jar ~/apps
+kill -15 $(ps aux | grep weblog-1.0-SNAPSHOT-runner.jar | awk '{print $2}')
 java -jar ~/apps/weblog-1.0-SNAPSHOT-runner.jar
