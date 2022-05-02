5b9faf3384e2cd1a40fd17227749b78c1a756912
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-05-02 23:23:02 +0800

    use mvnw

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cd ~/projects/weblog
 git pull
-mvn package -Dquarkus.package.type=uber-jar
+./mvnw package -Dquarkus.package.type=uber-jar
 cp ./target/weblog-1.0-SNAPSHOT-runner.jar ~/apps
 kill -15 $(ps aux | grep weblog-1.0-SNAPSHOT-runner.jar | awk '{print $2}')
 nohup java -jar ~/apps/weblog-1.0-SNAPSHOT-runner.jar > ~/apps/weblog.log 2>&1 &
